70494fd3a74b42f7c551bce962b2b671566988e8
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-10-27 23:58:22 +0000

    chore: Update driver code to test out debugging

--- main.go
+++ main.go
@@ -2,31 +2,39 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/nanafox/simple-http-client/pkg/client"
 )
 
 func main() {
-	apiClient := client.ApiClient{}
+	apiClient := client.ApiClient{Debug: true}
 
 	headers := []client.ApiHeader{
 		{Key: "Accept", Value: "application/json"},
 	}
 
-	url := "https://httpbin.org/get"
+	url := os.Getenv("API_URL")
+	if url == "" {
+		url = "https://httpbin.org/get"
+	}
 
 	queryParams := []client.ApiQuery{
 		{Key: "name", Value: "awesome"},
-		// Add as many as needed, it will handled automatically for you
+		// Add as many as needed, it will be handled automatically for you
 	}
 
 	apiClient.Get(url, queryParams, headers...)
 
 	if apiClient.Error != nil {
-		fmt.Println(apiClient.Error)
-		return
+		log.Fatal(apiClient.Error)
 	}
 
-	fmt.Println(apiClient.StatusCode)
-	fmt.Println(apiClient.Body)
+	// print the debug info for the request-response cycle
+	fmt.Println(apiClient.GetDebugInfo())
+
+	// the client keeps the response header information
+	fmt.Println("\n\nResponse Headers")
+	fmt.Println(apiClient.ResponseHeaders)
 }
